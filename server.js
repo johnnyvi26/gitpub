026188b25a8941c4c9df3779566aecc2f6ea28d7
johnnyvi26 <johnnymv26@gmail.com>
2021-09-26 23:40:56 -0400

    Making of index.ejs rout

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const drinks = require('./models/drinks');
+
+
 const app = express();
 const PORT = 3000;
 
@@ -8,6 +11,17 @@ app.get('/', (req, res)=>{
     res.send(`Welcome to the Gitpub App!`);
 });
 
+app.get('/drinks', (req,res)=>{
+    res.send(drinks);
+});
+
+
+app.get('/drinks/:idx', (req, res)=>{
+    res.render('drinks_index.ejs', {
+        drink: drinks[req.params.idx],
+        index: req.params.idx,
+    });
+});
 
 
 
